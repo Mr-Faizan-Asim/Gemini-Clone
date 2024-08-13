283509634efb763ca9e50e5c0178433e33bf60c4
Esa Irfan <144407847+esairfan@users.noreply.github.com>
2024-08-13 05:02:36 +0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import SideBar from './components/SideBar/SideBar'
- import Main from './components/Main/main'
+ import Main from './components/Main/Main'
  const App = () => {
    return (
      <>
